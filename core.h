b63841b071b5e71d25c8e203b2bd7dd664927e51
spch2008 <spch2008@foxmail.com>
2014-05-27 08:39:05 -0400

    core file

--- core.h
+++ core.h
@@ -3,12 +3,21 @@
 
 #include <sys/types.h>  
 #include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <sys/select.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string>
+#include <stdlib.h>
+#include <fcntl.h>
+
+class PoolManager;
 
 struct Connection
 {
     int fd;
     sockaddr client_addr;
 };
-
 
 #endif
